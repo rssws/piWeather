a7b30dce3af4300a077b5c68f0f530b73d747e3c
rssws <intlwangzp@gmail.com>
2022-06-24 22:50:59 +0200

    Remove pi api key for weather and ipGeolocation

--- piService/src/main/java/wang/zhongpin/pi/server/IPGeolocationController.java
+++ piService/src/main/java/wang/zhongpin/pi/server/IPGeolocationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import wang.zhongpin.pi.model.RequestLimitation;
 import wang.zhongpin.pi.model.Response;
 import wang.zhongpin.pi.model.ResponseStatus;
-import wang.zhongpin.pi.service.ApiKeyService;
 import wang.zhongpin.pi.service.IPGeolocationService.IPGeolocationAPI;
 import wang.zhongpin.pi.service.IPGeolocationService.IpApiComIPGeolocationAPI;
 import javax.servlet.http.HttpServletRequest;
@@ -14,24 +13,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/ipGeolocation")
 public class IPGeolocationController {
     private final IPGeolocationAPI ipGeolocationAPI;
-    private final ApiKeyService apiKeyService;
 
     RequestLimitation requestLimitationNormal = new RequestLimitation(1000 * 60, 30);
     RequestLimitation requestLimitationSensitive = new RequestLimitation(1000 * 60, 5);
 
     @Autowired
     public IPGeolocationController(
-            IpApiComIPGeolocationAPI ipApiComIPGeolocationAPI,
-            ApiKeyService apiKeyService) {
+            IpApiComIPGeolocationAPI ipApiComIPGeolocationAPI) {
         this.ipGeolocationAPI = ipApiComIPGeolocationAPI;
-        this.apiKeyService = apiKeyService;
     }
 
-    @GetMapping("/{apiKey}")
+    @GetMapping("/")
     public Response getIPGeolocationResponse(@PathVariable String apiKey, HttpServletRequest request) {
-        if(!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationNormal.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
@@ -43,15 +36,10 @@ public class IPGeolocationController {
         }
     }
 
-    @GetMapping("/{ip}/{apiKey}")
+    @GetMapping("/{ip}")
     public Response getIPGeolocationResponseByIP(
             @PathVariable String ip,
-            @PathVariable String apiKey,
             HttpServletRequest request) {
-
-        if (!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationSensitive.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
@@ -62,5 +50,4 @@ public class IPGeolocationController {
             return new Response(ResponseStatus.ERROR, e.getMessage());
         }
     }
-
 }
--- piService/src/main/java/wang/zhongpin/pi/server/WeatherController.java
+++ piService/src/main/java/wang/zhongpin/pi/server/WeatherController.java
@@ -13,34 +13,25 @@ import javax.servlet.http.HttpServletRequest;
 import static java.lang.Double.parseDouble;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/weather")
 public class WeatherController {
     private final WeatherAPI weatherAPI;
-    private final ApiKeyService apiKeyService;
 
     RequestLimitation requestLimitationNormal = new RequestLimitation(1000 * 60, 20);
     RequestLimitation requestLimitationSensitive = new RequestLimitation(1000 * 60, 10);
     RequestLimitation requestLimitationSensitive2 = new RequestLimitation(1000 * 60, 5);
 
-    // autowiring all beans that might be needed later...
-    // instead of using field injection, we leave the field just there and initialize it
-    // in the constructor, in which we want to inject the bean (not the constructor in bean itself!!!)
     @Autowired
     public WeatherController(
             OpenWeatherAPI openWeatherAPI,
             ApiKeyService apiKeyService) {
         this.weatherAPI = openWeatherAPI;
-        this.apiKeyService = apiKeyService;
     }
 
-    @GetMapping("/weather/city/{cityName}/{apiKey}")
+    @GetMapping("/city/{cityName}")
     public Response getWeatherResponseByCity(
             @PathVariable String cityName,
-            @PathVariable String apiKey,
             HttpServletRequest request) {
-        if(!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationNormal.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
@@ -52,15 +43,11 @@ public class WeatherController {
         }
     }
 
-    @GetMapping("/dailyWeather/coord/{lat}/{lon}/{apiKey}")
+    @GetMapping("/daily/coord/{lat}/{lon}")
     public Response getDailyWeatherResponseByCoord(
             @PathVariable String lat,
             @PathVariable String lon,
-            @PathVariable String apiKey,
             HttpServletRequest request) {
-        if(!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationSensitive.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
@@ -72,19 +59,14 @@ public class WeatherController {
         }
     }
 
-    @GetMapping("/hourlyWeather/coord/{lat}/{lon}/{apiKey}")
+    @GetMapping("/hourly/coord/{lat}/{lon}")
     public Response getHourlyWeatherResponseByCoord(
             @PathVariable String lat,
             @PathVariable String lon,
-            @PathVariable String apiKey,
             HttpServletRequest request) {
-        if(!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationSensitive.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
-
 
         try {
             return weatherAPI.getHourlyWeatherResponseByCoord(new Coord(parseDouble(lat), parseDouble(lon)));
@@ -93,13 +75,9 @@ public class WeatherController {
         }
     }
 
-    @GetMapping("/weather/ip/{apiKey}")
+    @GetMapping("/ip")
     public Response getWeatherResponseByIP(
-            @PathVariable String apiKey,
             HttpServletRequest request) {
-        if(!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationNormal.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
@@ -111,14 +89,10 @@ public class WeatherController {
         }
     }
 
-    @GetMapping("/weather/ip/{ip}/{apiKey}")
+    @GetMapping("/ip/{ip}")
     public Response getWeatherResponseByIP(
             @PathVariable String ip,
-            @PathVariable String apiKey,
             HttpServletRequest request) {
-        if(!apiKeyService.validate(apiKey)) {
-            return new Response(ResponseStatus.ERROR, "Api key is not valid!");
-        }
         if (requestLimitationSensitive2.isTooFrequent(Utils.getRemoteAddr(request))) {
             return new Response(ResponseStatus.ERROR, "Request sent too frequently! Please wait for one minute!");
         }
