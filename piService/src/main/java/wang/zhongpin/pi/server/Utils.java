702182beb804e849b404d691fa9107f6eee9119d
rssws <intlwangzp@gmail.com>
2023-01-29 14:12:01 +0100

    Remove faulty header for ip detection

--- piService/src/main/java/wang/zhongpin/pi/server/Utils.java
+++ piService/src/main/java/wang/zhongpin/pi/server/Utils.java
@@ -1,34 +1,23 @@
 package wang.zhongpin.pi.server;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import org.apache.commons.validator.routines.InetAddressValidator;
 import jakarta.servlet.http.HttpServletRequest;
 
 public final class Utils {
-    private static final String IPV4_PATTERN =
-            "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
-    private static final Pattern pattern = Pattern.compile(IPV4_PATTERN);
-
-    private static boolean isValidIpV4(final String s) {
-        if (s == null) {
-            return false;
-        }
-        Matcher matcher = pattern.matcher(s);
-        return matcher.matches();
+    private static boolean isValidAddr(final String s) {
+        InetAddressValidator inetAddressValidator = InetAddressValidator.getInstance();
+        return inetAddressValidator.isValid(s);
     }
 
     public static String getRemoteAddr(HttpServletRequest request) {
         String remoteAddr = "";
         if (request != null) {
-            remoteAddr = request.getHeader("True-Client-IP");
-            if (isValidIpV4(remoteAddr)) return remoteAddr;
             remoteAddr = request.getHeader("CF-Connecting-IP");
-            if (isValidIpV4(remoteAddr)) return remoteAddr;
+            if (isValidAddr(remoteAddr)) return remoteAddr;
             remoteAddr = request.getHeader("X-FORWARDED-FOR").split(",")[0];
-            if (isValidIpV4(remoteAddr)) return remoteAddr;
+            if (isValidAddr(remoteAddr)) return remoteAddr;
             remoteAddr = request.getRemoteAddr();
-            if (isValidIpV4(remoteAddr)) return remoteAddr;
+            if (isValidAddr(remoteAddr)) return remoteAddr;
             remoteAddr = "";
         }
         return remoteAddr;
